9775feb898c011d04b2c6a90c04f2f1f94c8d1c6
Gregor Müllegger <gregor@muellegger.de>
2016-03-22 16:29:07 +0100

    Add Python 3.5 as trove classifier

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
     ],
     zip_safe=False,
 )
